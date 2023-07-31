3fd3c8ef0e47a48ceb8004cb420f084be27bcd56
kvarad6 <kvarad6@gmail.com>
2023-07-31 15:35:25 +0530

    comments added

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from pandasai.llm.openai import OpenAI
 from pandasai import PandasAI
 
 #calling constructior
+#calling constructior
 load_dotenv()
 #Assessing openapi key from .env file
 open_api_key = os.getenv('OPEN_API_KEY')
